2eb94711a68f9979154297c0420a225aee32acd5
Kim Maida <yi.mihi@gmail.com>
2020-05-21 08:02:02 -0400

    Fix logging

--- outreach/schedule-followup.js
+++ outreach/schedule-followup.js
@@ -11,7 +11,7 @@ const schedule = {
     // Uncomment for testing: const timeout = 10000;
     timeoutCb = () =>  dmFollowup(app, recordObj);
     const followupID = setTimeout(timeoutCb, timeout);
-    console.log(`Scheduled followup for ${recordObj.name} in ${timeout / (1000 * 60 * 60)} hours: ${new Date(recordObj.followup_at)}`);
+    console.log(`Scheduled followup for ${recordObj.event_name} in ${timeout / (1000 * 60 * 60)} hours: ${new Date(recordObj.followup_at)}`);
   }
 };
 
