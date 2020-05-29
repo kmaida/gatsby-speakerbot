1328fed5917b5e35ae6ed4b078c0b339913a217a
Kim Maida <yi.mihi@gmail.com>
2020-05-29 13:22:18 -0400

    If followup time hasn't passed yet, schedule followup

--- data/airtable.js
+++ data/airtable.js
@@ -188,21 +188,25 @@ module.exports = {
     const followuptime = new Date(record.fields['Followup'] + 'T00:00:00Z').getTime();
     const hourDelay = (1000 * 60 * 60) * 16.5;  // 11:30/12:30 Eastern (depending on DST)
     const followupAt = followuptime + hourDelay;
-    const recordObj = {
-      id: record.getId(),
-      event_name: record.fields['Name'],
-      event_date: record.fields['Date'],
-      datetime: new Date(record.fields['Date'] + 'T00:00:00Z').getTime(),
-      followup_at: followupAt,
-      event_type: record.fields['Event Type'],
-      topic: record.fields['Topic'],
-      speakers: record.fields["Who's speaking?"],
-      url: record.fields['Event URL'],
-      submitterID: record.fields['Submitter Slack ID']
-    };
-    // Schedule the followup
-    schedule.followup(app, recordObj);
-    return recordObj;
+    const now = new Date().getTime();
+    // If followup time has not passed
+    if (now < followupAt) {
+      const recordObj = {
+        id: record.getId(),
+        event_name: record.fields['Name'],
+        event_date: record.fields['Date'],
+        datetime: new Date(record.fields['Date'] + 'T00:00:00Z').getTime(),
+        followup_at: followupAt,
+        event_type: record.fields['Event Type'],
+        topic: record.fields['Topic'],
+        speakers: record.fields["Who's speaking?"],
+        url: record.fields['Event URL'],
+        submitterID: record.fields['Submitter Slack ID']
+      };
+      // Schedule the followup
+      schedule.followup(app, recordObj);
+      return recordObj;
+    }
   },
 
   /*----
