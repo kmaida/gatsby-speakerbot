b6b534c1337476783743e0b34febbb3216273924
Kim Maida <yi.mihi@gmail.com>
2020-06-02 09:50:28 -0400

    Destructuring in userHome db, clear parameters

--- data/userhome-db.js
+++ data/userhome-db.js
@@ -15,7 +15,7 @@ const userHome = {
     @Param: userID
   ----*/
   async getUserHome(userID) {
-    return UserHome.findOne({userID = userID}, (err, userHome) => {
+    return UserHome.findOne({ userID }, (err, userHome) => {
       if (err) return dbErrHandler(err);
       if (!userHome) return new Error('No user home saved for this user');
       return userHome;
@@ -27,7 +27,7 @@ const userHome = {
     @Param: viewID
   ----*/
   async setUserHomeView(userID, viewID) {
-    return UserHome.findOne({ userID = userID }, (err, userHome) => {
+    return UserHome.findOne({ userID }, (err, userHome) => {
       if (err) return dbErrHandler(err);
       if (!submitReportID) return new Error('No report ID provided');
       // If userHome doesn't exist, create it
@@ -41,6 +41,19 @@ const userHome = {
           return newUserHome;
         });
       }
+      // If userHome exists but the viewID doesn't match, update it
+      else if (userHome.viewID !== viewID) {
+        userHome.viewID = viewID;
+        userHome.save((err) => {
+          if (err) return dbErrHandler(err);
+          console.log('Succesfully updated user\'s viewID', userHome.viewID);
+          return userHome;
+        })
+      }
+      // If userHome exists and viewID matches, return userHome
+      else {
+        return userHome;
+      }
     });
   },
   /*----
@@ -49,7 +62,7 @@ const userHome = {
     @Param: submitReportID
   ----*/
   async setSubmitReport(userID, submitReportID) {
-    return UserHome.findOne({ userID = userID }, (err, userHome) => {
+    return UserHome.findOne({ userID }, (err, userHome) => {
       if (err) return dbErrHandler(err);
       if (!submitReportID) return new Error('No report ID provided');
       userHome.submitReportID = submitReportID;
@@ -67,11 +80,35 @@ const userHome = {
     @Param: editReportID
   ----*/
   async setEditReport(userID, editReportID) {
-    return UserHome.findOne({ userID = userID }, (err, userHome) => {
+    return UserHome.findOne({ userID }, (err, userHome) => {
       if (err) return dbErrHandler(err);
       if (!editReportID) return new Error('No edit report ID provided');
       userHome.submitReportID = submitReportID;
       userHome.editReport = true;
+      // Save to database
+      userHome.save((err) => {
+        if (err) return dbErrHandler(err);
+        console.log('Successfully saved edit report ID', userHome.editReportID);
+        return userHome;
+      });
+    });
+  },
+  /*----
+    Clear param values when done with operations
+    @Param: userID
+    @Param: keys (single param string || array of params)
+  ----*/
+  async clearUserHomeParams(userID, keys) {
+    return UserHome.findOne({ userID }, (err, userHome) => {
+      if (err) return dbErrHandler(err);
+      if (!keys || !keys.length) return new Error('No keys provided to clear');
+      if (Array.isArray(keys)) {
+        keys.forEach((thisKey) => {
+          userHome[thisKey] = undefined;
+        });
+      } else if (typeof keys === 'string') {
+        userHome[keys] = undefined;
+      }
       // Save to database
       userHome.save((err) => {
         if (err) return dbErrHandler(err);
