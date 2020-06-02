afe178c5d0e7069b99b42253f6b999d6bf32bb3a
Kim Maida <yi.mihi@gmail.com>
2020-06-02 15:00:36 -0400

    #50 - abstract edit event action into its own file

--- events/app-home-opened.js
+++ events/app-home-opened.js
@@ -4,7 +4,8 @@ const homeBlocks = require('../bot-response/blocks-home/blocks-home');
 const errSlack = require('./../utils/error-slack');
 const triggerHomeViewUpdate = require('./../triggers/trigger-home-view-update');
 const blocksEventReport = require('./../bot-response/blocks-event-report');
-const blocksListEvent = require('./../bot-response/blocks-list-event');
+
+const actionEditEvent = require('./../triggers/action-edit-event');
 
 /*------------------
   APP HOME OPENED
@@ -193,49 +194,7 @@ const appHomeOpened = async (app, at) => {
     }
   });
 
-  /*----
-    ACTION: Edit an existing event report
-  ----*/
-  app.action('btn_edit_event', async ({ ack, body, context }) => {
-    await ack();
-    // If prefill info is available, set it
-    const prefill = body.actions ? JSON.parse(body.actions[0].value) : {};
-    // Create home parameters object
-    const settings = await store.getSettings();
-    const eventID = prefill ? prefill.id : undefined;
-    const localHomeParams = {
-      userID: body.user.id,
-      viewID: body.view.id,
-      botID: context.botUserId,
-      channel: settings.channel,
-      admins: settings.admins,
-      eventID: eventID
-    };
-    // Open post event report form
-    try {
-      const result = await app.client.views.open({
-        token: context.botToken,
-        trigger_id: body.trigger_id,
-        view: {
-          type: 'modal',
-          callback_id: 'list_event',
-          title: {
-            type: 'plain_text',
-            text: 'Edit Event Listing'
-          },
-          blocks: blocksListEvent(prefill),
-          private_metadata: JSON.stringify(localHomeParams),
-          submit: {
-            type: 'plain_text',
-            text: 'Update Event'
-          }
-        }
-      });
-    }
-    catch (err) {
-      errSlack(app, localHomeParams.userID, err);
-    }
-  });
+  actionEditEvent(app, store);
 }
 
 module.exports = appHomeOpened;
