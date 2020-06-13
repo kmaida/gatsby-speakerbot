180c2fc6339f93551b8ffe31835dc7ece6716bcc
Kim Maida <kim@kmaida.io>
2020-06-12 23:04:18 -0400

    #5 - Don't unfurl links in DM for event sync

--- bot-response/dm/dm-sync-events.js
+++ bot-response/dm/dm-sync-events.js
@@ -8,7 +8,8 @@ module.exports = async (app, userID, slackErr) => {
     const confirmSyncDM = await app.client.chat.postMessage({
       token: process.env.SLACK_BOT_TOKEN,
       channel: userID,
-      text: `:arrows_counterclockwise: *I've successfully synced with the <https://airtable.com/${process.env.AIRTABLE_TABLE_ID}/${process.env.AIRTABLE_VIEW_ID}|events in Airtable>.*\nI rescheduled followups for upcoming events and updated App Homes for active users.`
+      text: `:arrows_counterclockwise: *I've successfully synced with the <https://airtable.com/${process.env.AIRTABLE_TABLE_ID}/${process.env.AIRTABLE_VIEW_ID}|events in Airtable>.*\nI rescheduled followups for upcoming events and updated App Homes for active users.`,
+      unfurl_links: false
     });
   }
   catch (err) {
